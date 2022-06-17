a8973893b7788272751962dff55859092eb5ae2d
Pappu Rohit <rohitpappu23@gmail.com>
2022-06-17 19:20:11 +0530

    Added printArray()

--- utils.c
+++ utils.c
@@ -1,7 +1,18 @@
 #include "utils.h"
+#include <stdio.h>
 
 void swap(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void printArray(int *arr, int n, char separator) {
+  for (int i = 0; i < n - 1; i++) {
+    printf("%d%c ", arr[i], separator);
+  }
+  if (n > 0) {
+    // print last element
+    printf("%d\n", arr[n - 1]);
+  }
 }
--- utils.h
+++ utils.h
@@ -1,1 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 void swap(int* , int*);
+void printArray(int*, int, char);
+
+#endif
